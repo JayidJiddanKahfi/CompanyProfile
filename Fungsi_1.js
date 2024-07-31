7cdcd16f1faf5b3d3c7ef76f4f82b823187ff041
jayid Jiddan Kahfi <jayidjiddan231@gmail.com>
2024-07-31 10:07:34 +0700

    menambahkan fungsi pembagian

--- Fungsi_1.js
+++
@@ -1,5 +0,0 @@
-const penambahan = function (a, b) {
-    return a + b
-}
-
-console.log(penambahan(1, 3))
---
+++ Fungsi_tambah_kurang.js
@@ -0,0 +1,9 @@
+const penambahan = function (a, b) {
+    return a + b
+}
+console.log(penambahan(1, 3))
+
+const pengurangan = function (a, b) {
+    return a - b
+}
+console.log(pengurangan(3, 1))
